98807b7b2584a504bc4301e6ae231e4eea9d7fd8
andruw159 <andres.bernalm@udea.edu.co>
2023-10-05 23:10:53 -0500

    Graficación cartesiana y detalles de interfaz
    se agregó la gráficación cartesiana y se modificaron temas esteticos de la interfaz de usuario

--- Laboratorio2_1/Form1.Designer.cs
+++ Laboratorio2_1/Form1.Designer.cs
@@ -37,13 +37,17 @@
             this.labelAdvertencia2 = new System.Windows.Forms.Label();
             this.labelResultado = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ecUno
             // 
+            this.ecUno.BackColor = System.Drawing.Color.AliceBlue;
             this.ecUno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.ecUno.Location = new System.Drawing.Point(86, 103);
+            this.ecUno.Location = new System.Drawing.Point(88, 168);
             this.ecUno.Multiline = true;
             this.ecUno.Name = "ecUno";
             this.ecUno.Size = new System.Drawing.Size(318, 37);
@@ -53,8 +57,9 @@
             // 
             // ecDos
             // 
+            this.ecDos.BackColor = System.Drawing.Color.AliceBlue;
             this.ecDos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.ecDos.Location = new System.Drawing.Point(86, 228);
+            this.ecDos.Location = new System.Drawing.Point(88, 313);
             this.ecDos.Multiline = true;
             this.ecDos.Name = "ecDos";
             this.ecDos.Size = new System.Drawing.Size(318, 37);
@@ -66,7 +71,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label1.Location = new System.Drawing.Point(83, 71);
+            this.label1.Location = new System.Drawing.Point(183, 125);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(109, 25);
             this.label1.TabIndex = 2;
@@ -76,7 +81,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(83, 191);
+            this.label2.Location = new System.Drawing.Point(183, 273);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 25);
             this.label2.TabIndex = 3;
@@ -84,13 +89,15 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button1.Location = new System.Drawing.Point(172, 315);
+            this.button1.Location = new System.Drawing.Point(174, 406);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 33);
             this.button1.TabIndex = 4;
             this.button1.Text = "Resolver";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // labelAdvertencia1
@@ -98,7 +105,7 @@
             this.labelAdvertencia1.AutoSize = true;
             this.labelAdvertencia1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
             this.labelAdvertencia1.ForeColor = System.Drawing.Color.IndianRed;
-            this.labelAdvertencia1.Location = new System.Drawing.Point(415, 104);
+            this.labelAdvertencia1.Location = new System.Drawing.Point(417, 169);
             this.labelAdvertencia1.Name = "labelAdvertencia1";
             this.labelAdvertencia1.Size = new System.Drawing.Size(23, 36);
             this.labelAdvertencia1.TabIndex = 5;
@@ -109,7 +116,7 @@
             this.labelAdvertencia2.AutoSize = true;
             this.labelAdvertencia2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
             this.labelAdvertencia2.ForeColor = System.Drawing.Color.IndianRed;
-            this.labelAdvertencia2.Location = new System.Drawing.Point(415, 229);
+            this.labelAdvertencia2.Location = new System.Drawing.Point(417, 313);
             this.labelAdvertencia2.Name = "labelAdvertencia2";
             this.labelAdvertencia2.Size = new System.Drawing.Size(23, 36);
             this.labelAdvertencia2.TabIndex = 6;
@@ -119,7 +126,7 @@
             // 
             this.labelResultado.AutoSize = true;
             this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.labelResultado.Location = new System.Drawing.Point(404, 57);
+            this.labelResultado.Location = new System.Drawing.Point(565, 130);
             this.labelResultado.Name = "labelResultado";
             this.labelResultado.Size = new System.Drawing.Size(0, 20);
             this.labelResultado.TabIndex = 7;
@@ -127,16 +134,46 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(569, 191);
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(569, 168);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(354, 319);
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(107)))), ((int)(((byte)(135)))));
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Location = new System.Drawing.Point(82, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(775, 50);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ingrese dos ecuaciones para calcular su intersección, perpendicularidad o paralal" +
+    "elismo.\r\n                               Las ecuaciones debe ingresarlas de la fo" +
+    "rma y=mx+b.\r\n";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(107)))), ((int)(((byte)(135)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(980, 93);
+            this.panel1.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(982, 577);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.labelResultado);
@@ -147,10 +184,16 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ecDos);
             this.Controls.Add(this.ecUno);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,6 +210,8 @@
         private System.Windows.Forms.Label labelAdvertencia2;
         private System.Windows.Forms.Label labelResultado;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Laboratorio2_1/Form1.cs
+++ Laboratorio2_1/Form1.cs
@@ -19,20 +19,20 @@ namespace Laboratorio2_1
         {
             InitializeComponent();
             
-            
         }
+
         //Valida que la primera ecuación tenga un formato correcto.
         private void validarEcuacion1(object sender, EventArgs e)
         {
             string ecuacion1 = ecUno.Text;
             if (ecuacionValida(ecuacion1))
             {
-                // La ecuación es válida
+                // Cuando la ecuación es válida.
                 labelAdvertencia1.Visible = false; // Ocultar el símbolo de advertencia
             }
             else
             {
-                // La ecuación no es válida
+                // Cuando la ecuación no es válida.
                 labelAdvertencia1.Visible = true; // Mostrar el símbolo de advertencia
             }
         }
@@ -42,18 +42,17 @@ namespace Laboratorio2_1
             string ecuacion2 = ecDos.Text;
             if (ecuacionValida(ecuacion2))
             {
-                // La ecuación es válida
-                labelAdvertencia2.Visible = false; // Ocultar el símbolo de advertencia
+                // Cuando la ecuación es válida se oculta el símbolo de advertencia.
+                labelAdvertencia2.Visible = false;
 
             }
             else
             {
-                // La ecuación no es válida
-                labelAdvertencia2.Visible = true; // Mostrar el símbolo de advertencia
+                // Cuando la ecuación no es válida se muestra el símbolo de advertencia.
+                labelAdvertencia2.Visible = true;  
 
             }
         }
-        //---------------------------------------------------------------------------------------------------------
 
         //Funcion que extrae los valores de m y b, de las ecuaciones.
         private bool obtenerCoeficientes(string ecuacion, out double m, out double b)
@@ -63,42 +62,39 @@ namespace Laboratorio2_1
 
             try
             {
-                // Divide la ecuación en partes separadas por '='
-                string[] partes = ecuacion.Split('=');
+                // Reemplaza los espacios en blanco y convierte todo a minúsculas para simplificar la verificación
+                ecuacion = ecuacion.Replace(" ", "").ToLower();
 
-                if (partes.Length == 2 && partes[0].Trim().Equals("y", StringComparison.OrdinalIgnoreCase))
+                // Verifica si la ecuación comienza con "y=" y contiene "x" (ignora mayúsculas/minúsculas)
+                if (ecuacion.StartsWith("y=") && ecuacion.Contains("x"))
                 {
-                    string[] partesDerechas = partes[1].Split('+', '-');
-                    double coeficienteM = 0.0;
-                    double coeficienteB = 0.0;
+                    // Elimina "y=" y obtén la parte después del igual
+                    string parteDerecha = ecuacion.Substring(2);
 
-                    foreach (string termino in partesDerechas)
+                    // Divide la parte derecha en términos usando '+' o '-'
+                    string[] terminos = parteDerecha.Split(new char[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string termino in terminos)
                     {
-                        string terminoLimpio = termino.Trim();
-                        if (terminoLimpio.Contains("x"))
+                        // Revisa si el término contiene "x" y extrae el coeficiente (m)
+                        if (termino.Contains("x"))
                         {
-                            // El término contiene "x", por lo que es el coeficiente (m)
-                            if (terminoLimpio.Equals("x", StringComparison.OrdinalIgnoreCase))
-                            {
-                                coeficienteM = 1.0;
-                            }
-                            else
-                            {
-                                string coeficienteStr = terminoLimpio.Replace("x", "").Trim();
-                                coeficienteM = Convert.ToDouble(coeficienteStr);
-                            }
+                            string coeficienteStr = termino.Replace("x", "");
+
+                            // Convierte el coeficienteStr a double
+                            double coeficiente = Convert.ToDouble(coeficienteStr);
+                            m = coeficiente;
                         }
                         else
                         {
                             // El término no contiene "x", por lo que es el término constante (b)
-                            coeficienteB = Convert.ToDouble(terminoLimpio);
+                            b += Convert.ToDouble(termino);
                         }
                     }
 
-                    m = coeficienteM;
-                    b = coeficienteB;
                     return true;
                 }
+
                 return false;
             }
             catch
@@ -107,23 +103,24 @@ namespace Laboratorio2_1
             }
         }
 
-        //Gráfica
+
+        //Graficación cartesiana.
         private void pictureBoxGrafico_Paint(object sender, PaintEventArgs e)
         {
-            // Obtener el contexto de dibujo del PictureBox
+            // Obtener el contexto de dibujo del PictureBox.
             Graphics g = e.Graphics;
 
-            // Definir los límites del plano cartesiano
-            int minX = -10;
-            int maxX = 10;
-            int minY = -10;
-            int maxY = 10;
+            // Definir los límites del plano cartesiano.
+            int minX = -20;
+            int maxX = 20;
+            int minY = -20;
+            int maxY = 20;
 
-            // Obtener el tamaño del PictureBox
+            // Obtener el tamaño del PictureBox.
             int width = pictureBox1.ClientSize.Width;
             int height = pictureBox1.ClientSize.Height;
 
-            // Calcular la escala para convertir coordenadas del plano a coordenadas de píxeles
+            // Calcular la escala para convertir coordenadas del plano a coordenadas de píxeles.
             float scaleX = (float)width / (maxX - minX);
             float scaleY = (float)height / (minY - maxY);
 
@@ -140,11 +137,11 @@ namespace Laboratorio2_1
 
                 for (int x = 0; x < width; x++)
                 {
-                    double xValor = (x / scaleX) + minX;
-                    double yValor = m1 * xValor + b1;
+                    double xValor = (x - (width / 2)) / scaleX;
+                    double yValor = -(m1 * xValor + b1); // Negamos y para invertir la dirección del eje Y
 
                     int xPixel = x;
-                    int yPixel = (int)((height / 2) - (yValor * scaleY));
+                    int yPixel = (int)(height / 2 - yValor * scaleY);
                     puntosRecta1[x] = new Point(xPixel, yPixel);
                 }
 
@@ -160,11 +157,11 @@ namespace Laboratorio2_1
 
                 for (int x = 0; x < width; x++)
                 {
-                    double xValor = (x / scaleX) + minX;
-                    double yValor = m2 * xValor + b2;
+                    double xValor = (x - (width / 2)) / scaleX;
+                    double yValor = (m2 * xValor + b2); // Negamos y para invertir la dirección del eje Y
 
                     int xPixel = x;
-                    int yPixel = (int)((height / 2) - (yValor * scaleY));
+                    int yPixel = (int)(height / 2 - yValor * scaleY);
                     puntosRecta2[x] = new Point(xPixel, yPixel);
                 }
 
@@ -172,6 +169,7 @@ namespace Laboratorio2_1
             }
         }
 
+        //Validación del formato de las ecuaciones.
         private bool ecuacionValida(string ecuacion)
         {
             // Patrón de expresión regular para verificar la ecuación "y=mx+b"
@@ -190,15 +188,6 @@ namespace Laboratorio2_1
             return regex.IsMatch(ecuacion) && !ecuacion.Equals("y=", StringComparison.OrdinalIgnoreCase);
         }
 
-        private void ecUno_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                e.SuppressKeyPress = true; // Evita que se agregue una nueva línea en el TextBox
-                ecDos.Focus(); // Mueve el foco al siguiente TextBox (textBox2 en este caso)
-            }
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             // Asocia el evento Paint del PictureBox para dibujar las rectas en el plano cartesiano
@@ -208,7 +197,7 @@ namespace Laboratorio2_1
         
         private void ecUno_TextChanged(object sender, EventArgs e)
         {
-            //validamos condiciones de los valores ingresados
+            //Validamos condiciones de los valores ingresados
            
             validarEcuacion1(sender, e);
            
@@ -217,49 +206,79 @@ namespace Laboratorio2_1
 
         private void ecDos_TextChanged(object sender, EventArgs e)
         {
-            
+            //Validamos condiciones de los valores ingresados
             validarEcuacion2(sender, e);
             
             
         }
-        //Al presionar el boton se cálcula la intersección, paralelismo o perpendicularidad de las rectas.
+        //Al presionar el botón se cálcula la intersección, paralelismo o perpendicularidad de las rectas.
         public void button1_Click(object sender, EventArgs e)
         {
+            string ecuacion1 = ecUno.Text;
+            string ecuacion2 = ecDos.Text;
+
+            double m1;
+            double b1;
+            double m2;
+            double b2;
+
+            // Se extraen los coeficientes del string y se calcula la intersección si es que la tienen o su paralelismo o perpendicularidad.
+            if (obtenerCoeficientes(ecuacion1, out m1, out b1) && obtenerCoeficientes(ecuacion2, out m2, out b2))
             {
-                string ecuacion1 = ecUno.Text;
-                string ecuacion2 = ecDos.Text;
+                double xInterseccion = (b2 - b1) / (m1 - m2);
+                double yInterseccion = m1 * xInterseccion + b1;
 
-                double m1;
-                double b1;
-                double m2;
-                double b2;
+                labelResultado.Text = $"Intersección en ({xInterseccion} , {yInterseccion})";
 
-                if (obtenerCoeficientes(ecuacion1, out m1, out b1) && obtenerCoeficientes(ecuacion2, out m2, out b2))
+                bool m1Negativo = m1 < 0;
+                bool m2Negativo = m2 < 0;
+                bool b1Negativo = b1 < 0;
+                bool b2Negativo = b2 < 0;
+
+                // Verifica si las ecuaciones son coincidentes
+                if (Math.Abs(m1 - m2) < double.Epsilon && Math.Abs(b1 - b2) < double.Epsilon)
                 {
-                    double xInterseccion = (b2 - b1) / (m1 - m2);
-                    double yInterseccion = (m1 * xInterseccion) + b1;
-
-                    labelResultado.Text = $"Intersección en ({xInterseccion}, {yInterseccion})";
-
-                    if (m1 == m2)
+                    labelResultado.Text = "Las rectas son coincidentes";
+                }
+                else if (Math.Abs(m1 - m2) < double.Epsilon)
+                {
+                    if (m1Negativo && m2Negativo)
+                    {
+                        labelResultado.Text = $"Las rectas son paralelas en ({xInterseccion} , {yInterseccion}) con coeficientes negativos.";
+                    }
+                    else
                     {
                         labelResultado.Text = "Las rectas son paralelas";
                     }
-                    else if (m1 * m2 == -1)
+                }
+                else if (Math.Abs(m1 * m2 + 1) < double.Epsilon)
+                {
+                    if (m1Negativo && m2Negativo)
+                    {
+                        labelResultado.Text = $"Las rectas son perpendiculares en ({xInterseccion} , {yInterseccion}) con coeficientes negativos.";
+                    }
+                    else
                     {
                         labelResultado.Text = "Las rectas son perpendiculares";
                     }
                 }
                 else
                 {
-                    labelResultado.Text = "Las ecuaciones no están en el formato correcto.";
+                    labelResultado.Text = "Las rectas no son paralelas ni perpendiculares";
                 }
 
-                
+                // Invalidar el PictureBox para forzar la actualización de la gráfica.
+                pictureBox1.Invalidate();
+            }
+            else
+            {
+                labelResultado.Text = "Las ecuaciones no están en el formato correcto.";
             }
         }
+           private void label3_Click(object sender, EventArgs e)
+        {
 
-        
+        }
     }
     }
     
